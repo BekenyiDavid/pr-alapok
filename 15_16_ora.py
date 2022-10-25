8ea9a65e22bd96037bf82aa85eba7cf4b32585a7
BekenyiDavid <112931109+BekenyiDavid@users.noreply.github.com>
2022-10-25 09:38:28 +0200

    Add files via upload

---
+++ 15_16_ora.py
@@ -0,0 +1,39 @@
+""""
+print("Hello")
+print('Hello')
+
+
+a = "Hello"
+print(a)
+"""
+"""
+a = ""Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.""
+print(a)
+
+a = "Hello, World!"
+print(a[a])
+print(a[12])
+
+#a
+# b =10
+# b
+
+
+a = "hello world!asdfsdgake"
+
+
+print(a[len(a)-1])
+print("VEGE")
+print("az utolso", len(a)-1)
+"""
+
+a = "abcdefghijklmnop"
+
+szamlalo = 1
+for x in a:
+    if szamlalo % 2 == 0:
+        print(a[szamlalo-1], end="")
+    szamlalo = szamlalo+1
