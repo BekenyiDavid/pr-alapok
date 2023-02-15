9ee3dc5559e1fc04fcc7b9dbdea58f282deccaf3
BekenyiDavid <112931109+BekenyiDavid@users.noreply.github.com>
2023-02-15 08:00:53 +0100

    Add files via upload

---
+++ Python/Python/takypython/41_42_ora/feladat_2.py
@@ -0,0 +1,8 @@
+szavak = []
+szo = None
+while szo != "":
+    szo = str(input("Adj meg szavakat! Ha killépnél, a szó helyett csak egy ENTER-t üss! "))
+
+print("Program Vége")
+
+#Bekényi Dávid 2023.02.15. 7
