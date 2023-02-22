0f52d46ddcfe309bc8b4b172fd6703497989471f
BekenyiDavid <112931109+BekenyiDavid@users.noreply.github.com>
2023-02-22 07:37:08 +0100

    Delete Python directory

--- Python/Python/takypython/41_42_ora/feladat_2.py
+++
@@ -1,8 +0,0 @@
-szavak = []
-szo = None
-while szo != "":
-    szo = str(input("Adj meg szavakat! Ha killépnél, a szó helyett csak egy ENTER-t üss! "))
-
-print("Program Vége")
-
-#Bekényi Dávid 2023.02.15. 7
--- Python/takypython/9_10_ora/____ora.py
+++
@@ -1,4 +0,0 @@
-szam = input("Hogy hivnak?" )
-szam = int(szam)
-print(type(szam))
-print(szam)
